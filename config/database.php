b3911933b6966866ab227d7e2266f7b51f573d64
Gerardo Ponce <gerardoponcepari@gmail.com>
2021-03-30 02:07:19 -0500

    to production v0.3

--- config/database.php
+++ config/database.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Str;
 
 // For AWS DB
-define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
-define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
-define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
-define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
+$RDS_HOSTNAME = $_SERVER['RDS_HOSTNAME'];
+$RDS_USERNAME = $_SERVER['RDS_USERNAME'];
+$RDS_PASSWORD = $_SERVER['RDS_PASSWORD'];
+$RDS_DB_NAME = $_SERVER['RDS_DB_NAME'];
 
 return [
 
@@ -52,11 +52,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => RDS_HOSTNAME,
+            'host' => $RDS_HOSTNAME,
             'port' => env('DB_PORT', '3306'),
-            'database' => RDS_DB_NAME,
-            'username' => RDS_USERNAME,
-            'password' => RDS_PASSWORD,
+            'database' => $RDS_DB_NAME,
+            'username' => $RDS_USERNAME,
+            'password' => $RDS_PASSWORD,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
