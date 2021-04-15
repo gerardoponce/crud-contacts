e28fa176a8b38cf2918fcebb9d76a3e563de17d9
Gerardo Ponce <gerardoponcepari@gmail.com>
2021-04-15 00:21:46 -0500

    added docker-compose & Dockerfile

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthenticatedSessionController::class, 'create'])->middleware('guest');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
